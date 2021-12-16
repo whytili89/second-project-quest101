1241e91212480247045c7f9e5f85f3a9e6d66f8b
Hailey Park <haileypark@Haileys-MacBook-Pro.local>
2021-12-16 15:44:01 +0900

    KakaoLogin API Created

--- users/views.py
+++ users/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+import jwt, requests
 
-# Create your views here.
+from django.views import View
+from django.http  import JsonResponse
+from datetime     import datetime, timedelta
+
+from quest101.settings import SECRET_KEY, ALGORITHM
+from users.models      import User
+
+class KakaoAPI:
+    def __init__(self, access_token):
+        self.kakao_token = access_token
+        self.kakao_url = 'https://kapi.kakao.com/v2/user/me'
+
+    def get_kakao_user(self):
+        kakao_headers = {'Authorization' : f'Bearer {self.kakao_token}'}
+        response = requests.get(self.kakao_url, headers=kakao_headers, timeout = 5)
+    
+        if not response.status_code == 200:
+            return JsonResponse({'MESSAGE': 'INVALID_TOKEN'}, status=400)
+        
+        if response.status_code == -401:
+                return JsonResponse({'message': 'INVALID KAKAO USER'}, status=400)
+            
+        return response.json()
+
+class KakaoSignInView(View):
+    def get(self, request):
+        try:
+            kakao_token = request.headers.get('Authorization', None)
+            kakao_api   = KakaoAPI(kakao_token)
+            kakao_user  = kakao_api.get_kakao_user()
+            
+            kakao_id      = kakao_user['id']
+            name          = kakao_user['kakao_account']['profile']['nickname']
+            email         = kakao_user['kakao_account']['email']
+            profile_image = kakao_user['kakao_account']['profile']['thumbnail_image_url']
+            
+            user, created = User.objects.get_or_create(
+                kakao_id = kakao_id,
+                dafaults = {'name'          : name,
+                            'email'         : email,
+                            'profile_image' : profile_image
+                        }
+            )
+            
+            status_code  = 201 if created else 200
+        
+            access_token = jwt.encode({'user_id' : user.id, 'exp' : datetime.utcnow() + timedelta(days=7)}, SECRET_KEY, ALGORITHM)
+            
+            return JsonResponse({'message':'SUCCESS', 'access_token' : access_token}, status = status_code)
+        
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status = 400)
+        
+        except User.DoesNotExist:
+            return JsonResponse({'message':'INVALID_USER'}, status = 404)
+        
+        except jwt.ExpiredSignatureError:
+            return JsonResponse({'message':'TOKEN_EXPIRED'}, status = 400)
