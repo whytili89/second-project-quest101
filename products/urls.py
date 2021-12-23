a97a5fe5753aee8e2fdf8f8358c567c5abf4f42e
jaemoon <jaemoon406@gmail.com>
2021-12-23 18:23:05 +0900

    [Modify] OrderView's code

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import OrderView, CommentView
+from .views import CommentView, OrderView
 
 urlpatterns = [
-    path('/order/<int:course_id>', OrderView.as_view()),
-    path('/<int:course_id>/comments', CommentView.as_view())
+    path('/<int:course_id>/order', OrderView.as_view()),
+    path('/<int:course_id>/comment', CommentView.as_view())
 ]
